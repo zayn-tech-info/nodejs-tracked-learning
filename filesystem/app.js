0e42f604021ae1ce81d66249d4af95eb1da8b9db
zayn-tech-info <zayntech.info@gmail.com>
2025-08-05 15:31:31 -0300

    practicing readFile method

--- filesystem/app.js
+++ filesystem/app.js
@@ -6,3 +6,10 @@ console.log(input);
 const content = `This text was generated from from the input file: ${input} \n\n created at: ${new Date()}`;
 fs.writeFileSync("output.txt", content);
 
+fs.readFile("../filesystem/input.txt", "utf-8", (err, data) => {
+  console.log(data);
+  fs.readFile(`../filesystem/${data}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+  });
+});
+console.log("Reading file...");
