9b2f073683423c0490bf79b52a995f9f3169171d
zayn-tech-info <zayntech.info@gmail.com>
2025-08-05 12:55:49 -0300

    performing some logic on event emits

--- firstnodejs/app.js
+++ firstnodejs/app.js
@@ -10,6 +10,11 @@ rl.question("What is your name: ", (name) => {
   rl.close();
 });
 
+rl.on("close", () => {
+  console.log("Interface closed");
+  process.exit(0);
+});
+
 /* console.log(
   "Hello, i'm Zayn I just came back learning nodejs. I guess it's gonna be a great time haha"
 ); */
