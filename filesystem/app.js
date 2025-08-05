1320baea7d1867481f11fb041b0662a49ed44f19
zayn-tech-info <zayntech.info@gmail.com>
2025-08-05 13:22:45 -0300

    Write file

--- filesystem/app.js
+++ filesystem/app.js
@@ -1,4 +1,8 @@
-const fs = require('fs')
+const fs = require("fs");
 
-const input = fs.readFileSync('../filesystem/input.txt', 'utf-8')
+const input = fs.readFileSync("../filesystem/input.txt", "utf-8");
 console.log(input);
+
+const content = `This text was generated from from the input file: ${input} \n\n created at: ${new Date()}`;
+fs.writeFileSync("output.txt", content);
+
