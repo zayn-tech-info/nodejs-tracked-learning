e86a12a4b10c4854880b43c65b2aa4d92451d525
zayn-tech-info <zayntech.info@gmail.com>
2025-08-05 12:52:31 -0300

    reading input from command line interface

--- firstnodejs/app.js
+++ firstnodejs/app.js
@@ -1,1 +1,15 @@
-console.log("Hello, i'm Zayn I just came back learning nodejs. I guess it's gonna be a great time haha")
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("What is your name: ", (name) => {
+  console.log(`Your name is ${name}`);
+  rl.close();
+});
+
+/* console.log(
+  "Hello, i'm Zayn I just came back learning nodejs. I guess it's gonna be a great time haha"
+); */
