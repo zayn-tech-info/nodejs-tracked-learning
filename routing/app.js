232693caa9644f38c3a10bb6bb2b23df0685103e
zayn-tech-info <zayntech.info@gmail.com>
2025-08-06 19:02:18 -0300

    routing by reading some html file

--- routing/app.js
+++ routing/app.js
@@ -1,16 +1,19 @@
 const http = require("http");
+const fs = require("fs");
+
+const html = fs.readFileSync("../routing/index.html", "utf-8");
 
 const server = http.createServer((request, response) => {
   const path = request.url;
   if (path === "/" || path.toLowerCase() === "/home") {
-    response.end("You are in home page");
+    response.end(html.replace("{&VALUE&}", "You are in Home Page"));
   } else if (path.toLowerCase() === "/about") {
-    response.end("You are in About page");
+    response.end(html.replace("{&VALUE&}", "You are in About Page"));
   } else if (path.toLowerCase() === "/contact") {
-    response.end("You are in Contact page");
+    response.end(html.replace("{&VALUE&}", "You are in Contact Page"));
   } else {
     response.statusCode = 404;
-    response.end("404 Not Found");
+    response.end(html.replace("{&VALUE&}", "Error 404: Page not found"));
   }
 });
 
