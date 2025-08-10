7fdc26c298d98060eeeb91dfdbc9293a0fd9b785
zayn-tech-info <zayntech.info@gmail.com>
2025-08-10 23:21:33 +0100

    Understanding Streams

---
+++ stream/app.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const http = require("http");
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  let rs = fs.createReadStream("../stream/index.txt");
+
+  rs.on("data", (chunk) => {
+    res.write(chunk);
+  });
+
+  rs.on("end", () => {
+    res.end();
+  });
+  rs.on("error", (error) => {
+    res.end(error.message);
+  });
+});
+
+const PORT = 8000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on prot ${PORT}`);
+});
