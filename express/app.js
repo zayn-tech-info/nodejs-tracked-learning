f8020f745ff063ead8c8892fced74e54da0b18d2
zayn-tech-info <zayntech.info@gmail.com>
2025-08-15 22:22:04 +0100

    Get all movies

--- express/app.js
+++ express/app.js
@@ -1,18 +1,32 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
-const port = 3000;
+const movies = JSON.parse(fs.readFileSync("./movies.json", "utf-8"));
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello I'm from an express folder");
 });
 
-app.get("/", (req, res) => {
+// app.get("/", (req, res) => {
+//   res.status(200).json({
+//     message: "Hello I'm from an express folder",
+//     status: 200,
+//   });
+// });
+
+app.get("/api/v1/movies", (req, res) => {
   res.status(200).json({
-    message: "Hello I'm from an express folder",
-    status: 200,
+    status: "success",
+    count: movies.length,
+    data: {
+      movies,
+    },
   });
 });
+
+
+const port = 3000;
 
 app.listen(port, (req, res) => {
   console.log("Server already started...");
