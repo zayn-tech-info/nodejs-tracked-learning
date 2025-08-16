f9219dc78ea92781566ffdfc9a6dc6da07c762d8
zayn-tech-info <zayntech.info@gmail.com>
2025-08-16 03:49:50 +0100

    Performing CRUD operations

--- express/app.js
+++ express/app.js
@@ -4,16 +4,18 @@ const fs = require("fs");
 const app = express();
 const movies = JSON.parse(fs.readFileSync("./movies.json", "utf-8"));
 
+app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).send("Hello I'm from an express folder");
 });
 
-// app.get("/", (req, res) => {
-//   res.status(200).json({
-//     message: "Hello I'm from an express folder",
-//     status: 200,
-//   });
-// });
+/* app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Hello I'm from an express folder",
+    status: 200,
+  });
+});
+ */
 
 app.get("/api/v1/movies", (req, res) => {
   res.status(200).json({
@@ -25,6 +27,65 @@ app.get("/api/v1/movies", (req, res) => {
   });
 });
 
+app.post("/api/v1/movies", (req, res) => {
+  const newId = movies[movies.length - 1].id + 1;
+  console.log(newId);
+
+  const movie = Object.assign({ id: newId }, req.body);
+  movies.push(movie);
+
+  fs.writeFile("./movies.json", JSON.stringify(movies), (_, data) => {
+    res.status(201).json({
+      status: "success",
+      data: {
+        movie,
+      },
+    });
+  });
+});
+
+app.get("/api/v1/movies/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const movie = movies.find((movieItem) => movieItem.id === id);
+
+  if (!movie) {
+    res.status(404).json({
+      status: "success",
+      message: "Movie with this id " + id + " is not found",
+    });
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      movie,
+    },
+  });
+});
+
+app.patch("/api/v1/movies/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const movieToUpdate = movies.find((movieItem) => movieItem.id === id);
+
+  const movie = Object.assign(movieToUpdate, req.body);
+  const index = movies.indexOf(movieToUpdate);
+
+  if(!movie){
+    res.status(404).json({
+      status: 'fail',
+      message: 'Unable to update movie'
+    })
+  }
+  movies[index] = movieToUpdate;
+
+  fs.writeFile('./movies.json', JSON.stringify(movies), ()=>{
+    res.status(200).json({
+      status: 'success', 
+      data:{
+        movie
+      }
+    })
+  })
+});
 
 const port = 3000;
 
