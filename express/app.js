21c186079f3382859e7340c31079b81747a810a7
zayn-tech-info <zayntech.info@gmail.com>
2025-08-19 11:39:34 +0100

    finished the basis of nodejs

--- express/app.js
+++ express/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const productsRoute = require("./routes/products.routes");
 
 const app = express();
@@ -10,19 +9,6 @@ const timer = (req, res, next) => {
   req.requestedAT = new Date().toISOString();
   next();
 };
-
-mongoose
-  .connect(process.env.DB_CONNECTION_STRING, { 
-    useNewUrlParser: true,
-    // useUnifiedTopology: true 
-  })
-  .then((response) => {
-    // console.log(response);
-    console.log("Database connected succefully");
-  })
-  .catch((err) => {
-    console.log("An error occured: " + err);
-  });
 
 app.use(timer);
 
--- express/server.js
+++ express/server.js
@@ -1,12 +1,78 @@
+const mongoose = require("mongoose");
+
+const path = require("path");
 const dotenv = require("dotenv");
-dotenv.config({ path: ".env.local" });
+
+dotenv.config({ path: path.resolve(__dirname, "..", ".env.local") });
 
 const app = require("./app");
 const morgan = require("morgan");
+const { type } = require("os");
+
+mongoose
+  .connect(process.env.DB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    // useUnifiedTopology: true
+  })
+  .then((response) => {
+    // console.log(response);
+    console.log("Database connected succefully");
+  })
+  .catch((err) => {
+    console.log("An error occured: " + err);
+  });
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  categories: {
+    type: String,
+    trim: true,
+    enum: ["Electronics", "Clothing", "Food", "Other"],
+    required: true,
+  },
+  image: {
+    type: String,
+    required: [true, "Please provide an image for this product"],
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+    min: 0,
+    max: 5,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+const testProduct = new Product({
+  name: "Organic Honey Jar",
+  categories: "Food",
+  image: "https://example.com/images/organic-honey.jpg",
+  price: 25000,
+  ratings: 4.0,
+});
+
+testProduct
+  .save()
+  .then((docs) => {
+    console.log(docs);
+  })
+  .catch((err) => {
+    console.log("An error occoured : " + err);
+  });
 
 const port = process.env.PORT || 3000;
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 app.listen(port, (req, res) => {
