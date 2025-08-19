6db7291455d318e426e9fbe053e8bddba50256fe
zayn-tech-info <zayntech.info@gmail.com>
2025-08-19 10:10:09 +0100

    move the evn file outsite

--- express/app.js
+++ express/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const productsRoute = require("./routes/products.routes");
 
 const app = express();
@@ -9,6 +10,19 @@ const timer = (req, res, next) => {
   req.requestedAT = new Date().toISOString();
   next();
 };
+
+mongoose
+  .connect(process.env.DB_CONNECTION_STRING, { 
+    useNewUrlParser: true,
+    // useUnifiedTopology: true 
+  })
+  .then((response) => {
+    // console.log(response);
+    console.log("Database connected succefully");
+  })
+  .catch((err) => {
+    console.log("An error occured: " + err);
+  });
 
 app.use(timer);
 
